b1ce0a1770abb59652456a0002bdc66fbcdc6406
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-18 08:01:34 +0700

    menambahkan generic method

---
+++ PemrosesanData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODUL5
+{
+    class PemrosesanData<T>
+    {
+        T data;
+        public void DapatkanNilaiTerbesar<T>(T a, T b, T c)
+        {
+            dynamic data2 = b;
+            dynamic data3 = c;
+
+            dynamic biggest =a ;
+
+            if (biggest > data2 && biggest > data3)
+            {
+                biggest = biggest;
+            } else if (data2 > biggest && data2 > data3) {
+                biggest = data2;
+            } else if (data3 > biggest && data3 > data2)
+            {
+                biggest = data3;
+            }
+            
+            Console.WriteLine($"Terbesar adalah {biggest}");
+        }
+    }
+
+    class Generics
+    {
+        public static void Main(string[] Args)
+        {
+            PemrosesanData<double> data = new PemrosesanData<double>();
+            data.DapatkanNilaiTerbesar(1.0, 3.0, 2.2);
+        }
+    }
+}
--- obj/Debug/net8.0/MODUL5.AssemblyInfo.cs
+++ obj/Debug/net8.0/MODUL5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MODUL5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66647c48b2fa053f3c4f0a2591c54c7612aca89")]
 [assembly: System.Reflection.AssemblyProductAttribute("MODUL5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MODUL5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
